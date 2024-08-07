73aae44a7be1338f4c2797839608b37cc5c46452
oleksii.b <oleksii.b@dev.tagsoft.co>
2024-08-07 23:39:43 +0300

    refactored code style

--- load-keypair.ts
+++ load-keypair.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
-import { Keypair } from "@solana/web3.js";
+import {Keypair} from "@solana/web3.js";
 
 const secretKey = Uint8Array.from(JSON.parse(process.env["SECRET_KEY"]))
 const keypair = Keypair.fromSecretKey(secretKey);
 
 console.log(
-  `✅ Loaded keypair securely! Public key is: ${keypair.publicKey.toBase58()}`
+    `✅ Loaded keypair securely! Public key is: ${keypair.publicKey.toBase58()}`
 );
 
